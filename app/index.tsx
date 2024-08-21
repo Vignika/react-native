9a2315a66a2a3c8e1f2946233a533bb5bac5fba3
Vignika <20eg112132@anurag.edu.in>
2024-08-21 23:56:24 +0530

    images

--- app/index.tsx
+++ app/index.tsx
@@ -1,42 +1,15 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import DirectionLayout from "../components/directionLayout";
 
 const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-  },
-  bigBlue: {
-    color: 'blue',
-    fontWeight: 'bold',
-    fontSize: 30,
-  },
-  red: {
-    color: 'red',
-  },
+  
 });
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-     <DirectionLayout />
-
-<Text style={styles.bigBlue}>
-  Edit app/index.tsx to edit this screen.
-</Text>
-<View
-  style={{
-    width: 50,
-    height: 50,
-    backgroundColor: "powderblue",
-  }}
-/>
-</View>
+    <>
+    <Image source={require("../assets/images/react-logo.png")}/>
+    </>
 );
 
 }
