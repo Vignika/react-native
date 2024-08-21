6a1993a1c00cc781e860523ba59cc10afba94f60
Vignika <20eg112132@anurag.edu.in>
2024-08-21 11:31:25 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,29 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 50,
+  },
+  bigBlue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+});
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={styles.bigBlue}>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
